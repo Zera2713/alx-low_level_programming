866cef28cd266bd0b0fdc46cedcf96a47732fd9a
Zera2713 <zerabruk.mit@gmail.com>
2022-06-30 11:58:28 -0700

    Printing a message in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * main - program starts with
  * return : 0 error
  */
- 
+
  int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle"\");
+	puts("\"Programming is like building a multiligual puzzle");
 	return 0;
 }
