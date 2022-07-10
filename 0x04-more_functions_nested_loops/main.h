c1c046c9bf05073e457a48928a0639e84c91c073
Zera2713 <zerabruk.mit@gmail.com>
2022-07-10 00:52:08 -0700

    isdigit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,13 @@
 int _isupper(int c);
 int _putchar(char c);
 int main(void);
+int _isdigit(int c);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
 
