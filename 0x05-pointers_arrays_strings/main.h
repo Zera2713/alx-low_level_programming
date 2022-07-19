4074cb9ea0eb18e1098f60be3024a2e6c3c6f7e8
Zera2713 <zerabruk.mit@gmail.com>
2022-07-19 11:03:03 -0700

    Pointrs Arrays and Strings!

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+void reset_to_98(int *n);
+int main(void);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+int _putchar(char c);
+int _atoi(char *s);
+int _atoi(char *s);
+int _atoi(char *s);
+int find_sign(char *s);
+int idx_num_starts(char *s);
+int _strlen(char *s);
+
